0e20578571e63fd9e52f5669ed5857694defd282
gurmeet1011 <156289823+gurmeet1011@users.noreply.github.com>
2024-10-20 11:49:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function displayFormInfo() {
+            // Prevent form from submitting normally
+            // Retrieve form values
+            const firstName = document.getElementById('first-name').value;
+            const lastName = document.getElementById('last-name').value;
+            const phoneNumber = document.getElementById('phone').value;
+            const email = document.getElementById('email').value;
+
+            // Construct the alert message
+            const alertMessage = `First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phoneNumber} Email ID: ${email}`;
+
+            // Display the alert with form info
+            alert(alertMessage);
+        }
